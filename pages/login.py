4423eac1602557340bf522e70dd25a7c4fe151cc
Kaan Sezen <119515258+KaanSezen1923@users.noreply.github.com>
2025-01-19 23:45:59 +0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -7,7 +7,7 @@ import json
 
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(dict(st.secrets["firebase_config"]))
+    cred = credentials.Certificate("lotr-rag-fca1a-9073b2d36152.json")
     firebase_admin.initialize_app(cred)
 
 def login():
